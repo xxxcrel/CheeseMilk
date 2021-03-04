29cf998c3d2cd2542b7542ac77d62cb6fc487bbe
xxxcrel <crelxc@gmail.com>
2021-03-04 12:39:07 +0800

    update: refactor repository

--- src/main/java/beer/cheese/repository/PostRepository.java
+++ src/main/java/beer/cheese/repository/PostRepository.java
@@ -1,16 +1,18 @@
 package beer.cheese.repository;
 
-import beer.cheese.model.Category;
-import beer.cheese.model.Post;
-import beer.cheese.model.User;
+import java.time.LocalDateTime;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.time.LocalDateTime;
+import beer.cheese.model.Category;
+import beer.cheese.model.Post;
+import beer.cheese.model.User;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
 
@@ -22,10 +24,19 @@ public interface PostRepository extends JpaRepository<Post, Long> {
 
     @Modifying
     @Query("update Post p set p.commentCount = p.commentCount + :increment where p.id = :pid")
-    void updateComments(@Param("pid")Long pid, @Param("increment")int increment);
+    void updateComments(@Param("pid") Long pid, @Param("increment") int increment);
 
     @Modifying
     @Query("update Post p set p.starCount = p.starCount + :increment where p.id = :pid")
-    void updateStars(@Param("pid")Long pid, @Param("increment")int increment);
+    void updateStars(@Param("pid") Long pid, @Param("increment") int increment);
 
+    @EntityGraph(attributePaths = {"posts"}, type = EntityGraph.EntityGraphType.FETCH)
+    User findByNickname(String nickname);
+
+    @EntityGraph("User.postsFetchAll")
+    @Query("select u from User u where u.nickname = :nickname")
+    User fetchAllByNickname(@Param("nickname") String nickname);
+
+    @EntityGraph("User.postsFetchImages")
+    User getByNickname(String nickname);
 }
